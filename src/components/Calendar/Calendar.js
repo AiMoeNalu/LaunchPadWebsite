4d4ef25a55880c9735301622e38276c8603155be
AiMoeNalu <48229683+AiMoeNalu@users.noreply.github.com>
2019-06-28 10:33:22 -0400

    SignIn Component

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+/*
+const map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+*/
+
+const Calendar = () => {
+    return(
+        <div>
+            {'Calendar'}
+        </div>
+    );
+}
+
+export default Calendar;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import 'tachyons'
-
 const Footer = () => {
     return(
         <div 
         id="Footer"
-        className="fl w-100 pa2 mt3 ba">
-            FOOTER
+        className="w-100 pa2 mt3 ba">
+            {'FOOTER'}
         </div>
     );
 }
